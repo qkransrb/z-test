3cde771e05f94a8e24ccfe4ae42bc26bd1d7e46c
qkransrb <qkransrb90@gmail.com>
2024-07-22 10:54:07 +0900

    202407221054

---
+++ client/src/components/home/HomeSlider.js
@@ -0,0 +1,51 @@
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const HomeSlider = () => {
+  const settings = {
+    infinite: true,
+    fade: true,
+    speed: 1000,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    pauseOnHover: false,
+    arrows: false,
+  };
+
+  return (
+    <Slider {...settings} className="py-20">
+      <div className="bg-gradient-to-br from-primary/70 to-transparent h-[calc(100vh-400px)] !flex !flex-col !items-center !justify-center space-y-10 rounded-3xl">
+        <h2 className="text-6xl text-white font-semibold">ZYNORO Ecosystem</h2>
+        <p className="text-2xl text-white font-medium px-20 text-center">
+          Zynoro token is used as key currency for the recently launched QuantFi
+          service. Used for single and pair deposits. It also correlates closely
+          sith the Zynoro Governance of the Zynoro Ecosystem to achieve
+          ecosystem expansion.
+        </p>
+      </div>
+
+      <div className="bg-gradient-to-br from-primary/70 to-transparent h-[calc(100vh-400px)] !flex !flex-col !items-center !justify-center space-y-10 rounded-3xl">
+        <h2 className="text-6xl text-white font-semibold">ZYNORO Solution</h2>
+        <p className="text-2xl text-white font-medium px-20 text-center">
+          By developing quant algorithms (information, technical limitations,
+          user convenience, etc.) to solve the disproportionate environmental
+          problems of retail investors relative to corporate capital, we want to
+          provide solutions that enable retail investors to pursue stable
+          returns.
+        </p>
+      </div>
+
+      <div className="bg-gradient-to-br from-primary/70 to-transparent h-[calc(100vh-400px)] !flex !flex-col !items-center !justify-center space-y-10 rounded-3xl">
+        <h2 className="text-6xl text-white font-semibold">ZYNORO Mission</h2>
+        <p className="text-2xl text-white font-medium px-20 text-center">
+          It aims to become a community-based algorithm investment data
+          marketplace according to the trend of the blockchain industry.
+        </p>
+      </div>
+    </Slider>
+  );
+};
+
+export default HomeSlider;
--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -5,6 +5,8 @@ import PoolList from "../components/home/PoolList";
 import TVL from "../components/home/TVL";
 import Title from "../components/Title";
 import { fetchDefaultAction } from "../redux/actions/defaultAction";
+import HomeSlider from "../components/home/HomeSlider";
+import zynoro from "../assets/videos/zynoro.mp4";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -23,20 +25,39 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="space-y-7 pb-14">
-      <Title title="homeScreen.title" />
-      <div className="flex flex-col lg:flex-row lg:space-x-6 space-y-7 lg:space-y-0">
-        <div className="w-full lg:w-[60%]">
-          <TVL />
+    <>
+      <HomeSlider />
+
+      <video
+        muted
+        autoPlay
+        loop
+        // className="xl:fixed top-0 left-0 w-full xl:opacity-70 mt-20 mb-40 xl:my-0 rounded-md xl:rounded-none"
+        className="max-w-[900px] w-full mx-auto pb-20"
+      >
+        <source src={zynoro} type="video/mp4" />
+      </video>
+      <div className="space-y-7 pb-20">
+        <Title title="homeScreen.title" />
+        <div className="flex flex-col lg:flex-row lg:space-x-6 space-y-7 lg:space-y-0">
+          <div className="w-full lg:w-[60%]">
+            <TVL />
+          </div>
+          <div className="w-full lg:w-[40%]">
+            <Circulation />
+          </div>
         </div>
-        <div className="w-full lg:w-[40%]">
-          <Circulation />
+        <div>
+          <PoolList />
         </div>
+        <img
+          src="/images/allocation.svg"
+          alt=""
+          width={900}
+          className="mx-auto py-20"
+        />
       </div>
-      <div>
-        <PoolList />
-      </div>
-    </div>
+    </>
   );
 };
 
