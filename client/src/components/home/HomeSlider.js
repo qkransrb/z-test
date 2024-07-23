7f30f723d90ff9b2675c320e564d6426519dd890
qkransrb <qkransrb90@gmail.com>
2024-07-23 15:37:09 +0900

    202407231537

--- client/src/components/home/HomeSlider.js
+++ client/src/components/home/HomeSlider.js
@@ -16,9 +16,11 @@ const HomeSlider = () => {
 
   return (
     <Slider {...settings} className="py-20">
-      <div className="bg-gradient-to-br from-primary/70 to-transparent h-[calc(100vh-400px)] !flex !flex-col !items-center !justify-center space-y-10 rounded-3xl">
-        <h2 className="text-6xl text-white font-semibold">ZYNORO Ecosystem</h2>
-        <p className="text-2xl text-white font-medium px-20 text-center">
+      <div className="bg-gradient-to-br from-primary/70 to-transparent h-[600px] !flex !flex-col !items-center !justify-center space-y-10 rounded-3xl">
+        <h2 className="text-3xl md:text-6xl text-white font-semibold">
+          ZYNORO Ecosystem
+        </h2>
+        <p className="text-lg md:text-2xl text-white font-medium px-20 text-center">
           Zynoro token is used as key currency for the recently launched QuantFi
           service. Used for single and pair deposits. It also correlates closely
           sith the Zynoro Governance of the Zynoro Ecosystem to achieve
@@ -26,9 +28,11 @@ const HomeSlider = () => {
         </p>
       </div>
 
-      <div className="bg-gradient-to-br from-primary/70 to-transparent h-[calc(100vh-400px)] !flex !flex-col !items-center !justify-center space-y-10 rounded-3xl">
-        <h2 className="text-6xl text-white font-semibold">ZYNORO Solution</h2>
-        <p className="text-2xl text-white font-medium px-20 text-center">
+      <div className="bg-gradient-to-br from-primary/70 to-transparent h-[600px] !flex !flex-col !items-center !justify-center space-y-10 rounded-3xl">
+        <h2 className="text-3xl md:text-6xl text-white font-semibold">
+          ZYNORO Solution
+        </h2>
+        <p className="text-lg md:text-2xl text-white font-medium px-20 text-center">
           By developing quant algorithms (information, technical limitations,
           user convenience, etc.) to solve the disproportionate environmental
           problems of retail investors relative to corporate capital, we want to
@@ -37,9 +41,11 @@ const HomeSlider = () => {
         </p>
       </div>
 
-      <div className="bg-gradient-to-br from-primary/70 to-transparent h-[calc(100vh-400px)] !flex !flex-col !items-center !justify-center space-y-10 rounded-3xl">
-        <h2 className="text-6xl text-white font-semibold">ZYNORO Mission</h2>
-        <p className="text-2xl text-white font-medium px-20 text-center">
+      <div className="bg-gradient-to-br from-primary/70 to-transparent h-[600px] !flex !flex-col !items-center !justify-center space-y-10 rounded-3xl">
+        <h2 className="text-3xl md:text-6xl text-white font-semibold">
+          ZYNORO Mission
+        </h2>
+        <p className="text-lg md:text-2xl text-white font-medium px-20 text-center">
           It aims to become a community-based algorithm investment data
           marketplace according to the trend of the blockchain industry.
         </p>
--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -33,11 +33,11 @@ const Home = () => {
         autoPlay
         loop
         // className="xl:fixed top-0 left-0 w-full xl:opacity-70 mt-20 mb-40 xl:my-0 rounded-md xl:rounded-none"
-        className="max-w-[900px] w-full mx-auto pb-20"
+        className="max-w-[900px] w-full mx-auto pt-20 pb-20"
       >
         <source src={zynoro} type="video/mp4" />
       </video>
-      <div className="space-y-7 pb-20">
+      <div className="space-y-7">
         <Title title="homeScreen.title" />
         <div className="flex flex-col lg:flex-row lg:space-x-6 space-y-7 lg:space-y-0">
           <div className="w-full lg:w-[60%]">
@@ -54,7 +54,7 @@ const Home = () => {
           src="/images/allocation.svg"
           alt=""
           width={900}
-          className="mx-auto py-20"
+          className="mx-auto py-48"
         />
       </div>
     </>
