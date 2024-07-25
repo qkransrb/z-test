591c5ec65fadd5427b0cf3985a620628046a6e36
qkransrb <qkransrb90@gmail.com>
2024-07-25 10:22:05 +0900

    202407251022

--- client/src/screens/PlayToEarn.js
+++ client/src/screens/PlayToEarn.js
@@ -1,7 +1,6 @@
 import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import AnimaOfQuantmix from "../assets/videos/anima.mp4";
 import { BiLinkExternal } from "react-icons/bi";
 
 const P2E = () => {
@@ -9,15 +8,6 @@ const P2E = () => {
 
   return (
     <Fragment>
-      <video
-        muted
-        autoPlay
-        loop
-        className="xl:fixed top-0 left-0 w-full xl:opacity-70 mt-20 mb-40 xl:my-0 rounded-md xl:rounded-none"
-      >
-        <source src={AnimaOfQuantmix} type="video/mp4" />
-      </video>
-
       <div className="md:absolute bottom-28 right-10 lg:bottom-20 lg:right-32 xl:bottom-32 xl:right-44 flex flex-col justify-center items-center space-y-10">
         <div className="flex gap-4">
           <div className="relative group text-xl md:text-2xl min-w-[150px] md:min-w-[200px] text-center text-[#246bea] bg-white rounded-full shadow-lg font-bold px-6 py-1 md:py-2 hover:text-white hover:bg-[#246bea] duration-300 cursor-pointer">
